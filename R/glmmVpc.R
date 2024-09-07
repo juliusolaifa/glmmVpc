52863302ac2ad75962d99d4d52a71727de0f2a97
juliusolaifa <jbolaifa@gmail.com>
2024-09-07 04:51:41 -0500

    preserving the Feature number even when fit object is null

--- R/glmmVpc.R
+++ R/glmmVpc.R
@@ -203,7 +203,7 @@ coef.Glmmfit <- function(object, ...) {
 
   # Combine the coefficients into a data frame
   coefs <- data.frame(do.call(rbind, coef_lists))
-  rownames(coefs) <- paste0("Feature", 1:nrow(coefs))
+  rownames(coefs) <- paste0("Feature", which(!sapply(coef_lists, is.null)))
 
   return(coefs)
 }
