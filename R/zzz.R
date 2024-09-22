1a1fa1349c92820caec56d95927007344ba67f81
juliusolaifa <jbolaifa@gmail.com>
2024-09-21 22:04:35 -0500

    updating the onLoad function

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,15 @@
-.onLoad <- function(libname, pkgname) {
-  bioc_pkgs <- c("BiocParallel", "DESeq2", "S4Vectors", "SummarizedExperiment")
-  for (pkg in bioc_pkgs) {
-    if (!requireNamespace(pkg, quietly = TRUE)) {
-      BiocManager::install(pkg)
-    }
+.onAttach <- function(libname, pkgname) {
+  required_pkgs <- c("BiocManager", "BiocParallel", "DESeq2", "S4Vectors", "SummarizedExperiment")
+  missing_pkgs <- required_pkgs[!sapply(required_pkgs, requireNamespace, quietly = TRUE)]
+
+  if (length(missing_pkgs) > 0) {
+    msg <- paste0(
+      "Some required packages are not installed: ",
+      paste(missing_pkgs, collapse = ", "),
+      ". \nPlease install them using: \n",
+      "if (!requireNamespace('BiocManager', quietly = TRUE)) install.packages('BiocManager')\n",
+      "BiocManager::install(c('", paste(missing_pkgs, collapse = "', '"), "'))"
+    )
+    packageStartupMessage(msg)
   }
 }
