03573f631573316684dde22bbf4a71a3cf862565
juliusolaifa <jbolaifa@gmail.com>
2024-09-28 21:11:44 -0500

    non standard chi square

--- R/vpc_test.R
+++ R/vpc_test.R
@@ -60,7 +60,6 @@ vpc.test.Glmmfits <- function(fitObj, null_formula = NULL, test_type = c("LRT", 
 }
 
 
-
 #' Perform Variance Partition Coefficient (VPC) Test
 #'
 #' This function calculates the Variance Partition Coefficient (VPC) for a fitted model object.
@@ -79,4 +78,138 @@ vpc.test.Glmmfits <- function(fitObj, null_formula = NULL, test_type = c("LRT", 
 vpc.test <- function(fitObj, null_formula = NULL, test_type = c("LRT", "Score")) {
   UseMethod("vpc.test")
 }
+
+
+#' Adjusted Chi-Square Test Statistic
+#'
+#' This function calculates the p-value for a chi-square test statistic based on the
+#' configuration parameters passed in `config`. It supports different cases for
+#' parameters on and off the boundary.
+#'
+#' @param test_stat Numeric value representing the chi-square test statistic.
+#' @param config A list containing the configuration parameters:
+#' \describe{
+#'   \item{q}{Parameter of interest on the boundary.}
+#'   \item{r}{Parameter of interest not on the boundary.}
+#'   \item{s}{Nuisance parameter on the boundary (in this function, it is always 0).}
+#'   \item{t}{Nuisance parameter not on the boundary.}
+#' }
+#'
+#' @return A numeric value representing the calculated p-value based on the
+#' specified configuration.
+#'
+#' @details
+#' The function evaluates the test statistic based on three cases:
+#' \describe{
+#'   \item{Case I}{If \code{q == 0} and \code{s == 0}, it calculates the p-value
+#'   using \code{r} degrees of freedom.}
+#'   \item{Case II}{If \code{q == 1}, \code{r == 0}, and \code{s == 0}, it
+#'   calculates a weighted p-value combining \code{df = 1} and \code{test_stat == 0}.}
+#'   \item{Case III}{If \code{q == 1} and \code{s == 0}, it calculates a weighted
+#'   p-value using degrees of freedom \code{r - 1} and \code{r}.}
+#' }
+#'
+#' @references
+#' Self, S.G., & Liang, K. (1987). *On Asymptotic properties of maximum
+#' likelihood estimators
+#' and likelihood ratio tests under non-standard conditions*.
+#' Journal of American Statistical Association, 82(398), 605-610-145.
+#' \url{https://www.jstor.org/stable/2289471}
+#'
+#' @export
+adj_chisq <- function(test_stat, config) {
+
+  # Ensure config has all the necessary components
+  if (is.null(config$q) || is.null(config$r) || is.null(config$s) || is.null(config$t)) {
+    stop("Config must contain 'q', 'r', 's', and 't'.")
+  }
+
+  calculate_p_value <- function(q, r, s, test_stat) {
+    caseI <- q == 0 && s == 0
+    caseII <- q == 1 && r == 0 && s == 0
+    caseIII <- q == 1 && s == 0
+
+    if (caseI)   {
+      return(stats::pchisq(test_stat, df = r, lower.tail = FALSE))
+    } else if (caseII) {
+      return(0.5 * stats::pchisq(test_stat, df = 1, lower.tail = FALSE) +
+               0.5 * (test_stat == 0))
+    } else if (caseIII) {
+      df1 <- r - 1
+      df2 <- r
+      return(0.5 * stats::pchisq(test_stat, df = df1, lower.tail = FALSE) +
+               0.5 * stats::pchisq(test_stat, df = df2, lower.tail = FALSE))
+    } else {
+      stop("Invalid configuration for q, r, s.")
+    }
+  }
+
+  p_value <- calculate_p_value(config$q, config$r, config$s, test_stat)
+
+  return(p_value)
+}
+
+
+
+#' Obtain Configuration Parameters for Boundary and Non-Boundary Models
+#'
+#' This function calculates the parameters `q`, `r`, `s`, and `t` based on the
+#' difference
+#' between a null and alternative model, particularly in cases where certain
+#' parameters
+#' are on the boundary or not.
+#'
+#' @param null A model object representing the null hypothesis. This object
+#' should contain
+#'   `modObj` and `Sigma`, where `modObj` is the model object and `Sigma` is a
+#'   matrix indicating
+#'   the parameters on the boundary.
+#' @param alt A model object representing the alternative hypothesis. This
+#' object should contain
+#'   `modObj` and `Sigma`, where `modObj` is the model object and `Sigma` is a
+#'   matrix indicating
+#'   the parameters on the boundary.
+#'
+#' @return A list with the following components:
+#' \describe{
+#'   \item{q}{Number of parameters of interest that are on the boundary in the
+#'   alternative model but not in the null model.}
+#'   \item{r}{Number of parameters of interest that are not on the boundary in
+#'   either the null or alternative model.}
+#'   \item{s}{Number of nuisance parameters on the boundary (always 0 in these
+#'   cases).}
+#'   \item{t}{Number of nuisance parameters not on the boundary.}
+#' }
+#'
+#' @references
+#' Self, S.G., & Liang, K. (1987). *On Asymptotic properties of maximum
+#' likelihood estimators
+#' and likelihood ratio tests under non-standard conditions*.
+#' Journal of American Statistical Association, 82(398), 605-610-145.
+#' \url{https://www.jstor.org/stable/2289471}
+#'
+#' @export
+obtain_config <- function(null, alt){
+
+  # q : par of interest on the boundary
+  # r : par of interest not on boundary
+  # s : nuisance par on the boundary (for our use cases this is always `0`)
+  # t : nuisance par not on the boundary
+  p <- full_par.alt <- nrow(stats::vcov(alt$modObj, full=TRUE))
+  full_par.null <- nrow(stats::vcov(null$modObj, full=TRUE))
+
+  bounded_par.alt <- nrow(alt$Sigma)
+  bounded_par.null <- nrow(null$Sigma)
+
+  non_bounded.alt  <- full_par.alt - bounded_par.alt
+  non_bounded.null <- full_par.null - bounded_par.null
+
+  q <- bounded_par.alt - bounded_par.null
+  r <- non_bounded.alt - non_bounded.null
+  s <- 0
+  t <- p-q-r-s
+  return(list("q"=q,"r"=r,"s"=s,"t"=t))
+}
+
+
 
